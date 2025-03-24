89e6d6dea118fb31ddf455fd348f1b0c36463880
jayc722 <jaewon8469@gmail.com>
2025-03-24 14:23:05 +0900

    pw 암호화

--- src/main/java/kr/kh/tmp/service/MemberServiceImp.java
+++ src/main/java/kr/kh/tmp/service/MemberServiceImp.java
@@ -1,6 +1,7 @@
 package kr.kh.tmp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.kh.tmp.dao.MemberDAO;
@@ -12,6 +13,9 @@ public class MemberServiceImp implements MemberService {
 	@Autowired
 	MemberDAO memberDAO;
 
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
 	@Override
 	public boolean signup(MemberVO member) {
 		if(member==null) {
@@ -20,6 +24,9 @@ public class MemberServiceImp implements MemberService {
 		//아이디 비번 유효성검사(했다치고)
 		
 		
+		//비번 암호화
+		String encPw = passwordEncoder.encode(member.getMe_pw());
+		member.setMe_pw(encPw);
 		return memberDAO.insertMember(member);
 	}
 }
