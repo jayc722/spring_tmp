5cdfef8ea0c67e254687e96e9ce89a40d52ed7f3
jayc722 <jaewon8469@gmail.com>
2025-03-24 16:37:35 +0900

    게시글 등록 위한 준비.

---
+++ src/main/java/kr/kh/tmp/dao/PostDAO.java
@@ -0,0 +1,11 @@
+package kr.kh.tmp.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.kh.tmp.model.vo.MemberVO;
+
+public interface PostDAO {
+
+	
+	
+}
---
+++ src/main/java/kr/kh/tmp/model/vo/PostVO.java
@@ -0,0 +1,9 @@
+package kr.kh.tmp.model.vo;
+
+import lombok.Data;
+
+@Data
+public class PostVO {
+	
+
+}
